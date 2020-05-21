c81b097a8d8682095797158b6c6fbdf07c54316c
Arman Riahi <namrataffy@gmail.com>
2020-05-21 12:57:15 -0700

    backend is set up, can connect to db

---
+++ seeders/seeds.js
@@ -0,0 +1,22 @@
+var db = require("../models");
+
+let users = [
+  {
+    user_name: "Arman_user",
+    password: "password",
+    first_name: "Arman",
+    last_name: "Riahi",
+  },
+  {
+    user_name: "Chris_user",
+    password: "password",
+    first_name: "Chris",
+    last_name: "Melby",
+  },
+  {
+    user_name: "JDawg",
+    password: "password",
+    first_name: "Jerome",
+    last_name: "Chenette",
+  },
+];
--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 var express = require("express");
 var session = require("express-session");
 var path = require("path");
-var db = require("./models");
 
-var PORT = 3001;
+var PORT = process.env.PORT || 3001;
+var db = require("./models");
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("uploads"));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-require("./routes/food-api-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-require("./routes/meal-api-routes.js")(app);
-require("./routes/health-api-routes.js")(app);
+require("./routes/food-api-route.js")(app);
+require("./routes/user-api-route.js")(app);
+require("./routes/meal-api-route.js")(app);
+require("./routes/health-api-route.js")(app);
 
 app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
+    // require("./seeders/seeds")();
     console.log("App listening on PORT " + PORT);
   });
 });
